951405e1c93b1e0927409f030350578b8a07afcd
Sebastian Skoczylas <s.skoczylas92@gmail.com>
2019-10-24 18:59:48 +0200

    Correct version

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Hero.scss';
+import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
+
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <header className = {styles.component}>
+        <h2 className={styles.title}>{ReactHtmlParser(this.props.titleText)}</h2>
+        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"/>
+      </header>
+    )
+  }
+}
+
+Hero.propTypes = {
+  titleText: PropTypes.node,
+};
+
+
+export default Hero;
